222b1ab91c702644a868051190050c8e0ac8d78d
isabella Hu <eunicejhu@gmail.com>
2016-10-11 09:45:54 +0200

    change routes

--- routes.js
+++ routes.js
@@ -3,12 +3,11 @@ let
 	configRoutes,
 	mongodb = require('mongodb'),
 	mongoServer,
-	dbHandle;
+	dbHandle,
+	makeMongoId;
 mongoServer = new mongodb.Server('localhost', 27017);
 dbHandle = new mongodb.Db('spa', mongoServer, {safe: true});
-dbHandle.open(() => {
-	console.log('*** Connected to MongoDb ***');
-});
+makeMongoId = mongodb.ObjectID;
 
 configRoutes = (app, server) => {
 	app.get('/', (request, response) => {
@@ -16,40 +15,117 @@ configRoutes = (app, server) => {
 	});
 
 	//interceptor
-	app.all('/user/*?', (request, response, next) => {
+	app.all('/:obj_type/*?', (request, response, next) => {
+		console.log(`${request.params.url} all`);
 		response.set('Content-Type', 'application/json');
 		next();
 	});
 
 	app.get('/:obj_type/list', (request, response) => {
-		response.send({
-			title: `${request.params.obj_type} list`
-		});
+		dbHandle.collection(
+			request.params.obj_type,
+			(outer_error, collection) => {
+				collection.find().toArray(
+					(inner_error, map_list) => {
+						response.send(map_list);
+					}
+				);
+			}
+		);
 	});
 
 	app.post('/:obj_type/create', (request, response) => {
-		response.send({
-			title: `${request.params.obj_type} created`
-		});
+		console.log("create, ", request.params.obj_type);
+		dbHandle.collection(
+			request.params.obj_type,
+			(outer_error, collection) => {
+				let
+					options_map = {safe: true},
+					obj_map = request.body;
+				collection.insert(
+					obj_map,
+					options_map,
+					(inner_error, result_map) => {
+						response.send(result_map);
+					}
+				);
+			}
+		);
 	});
 
-	app.get('/:obj_type/read/:id([0-9]+)', (request, response) => {
-		response.send({
-			title: `${request.params.obj_type} with id ${request.params.id} Found`
-		})
+	app.get('/:obj_type/read/:id', (request, response) => {
+		let 
+			find_map = {
+				_id: makeMongoId(request.params.id)
+			};
+		dbHandle.collection(
+			request.params.obj_type,
+			(outer_error, collection) => {
+				collection.findOne(
+					find_map,
+					(inner_error, result_map) => {
+						response.send(result_map);
+					}
+				);
+			}
+		);
 	});
 
-	app.post('/:obj_type/update/:id([0-9]+)', (request, response) => {
-		response.send({
-			title: `${request.params.obj_type} with id ${request.params.id} updated`
-		})
+	app.post('/:obj_type/update/:id', (request, response) => {
+		let
+			find_map = {
+				_id: makeMongoId(request.params.id)
+			},
+			obj_map = request.body;
+		dbHandle.collection(
+			request.params.obj_type,
+			(outer_error, collection) => {
+				let
+					options_map = {
+						"new" : true,
+						"upsert": false,
+						"safe": true
+					};
+				//findAndModify is deprecated
+				collection.findOneAndUpdate(
+					find_map,
+					obj_map,
+					options_map,
+					(inner_error, updated_map) => {
+						response.send(updated_map);
+					}
+				);
+			}
+		);
 	});
 
-	app.get('/:obj_type/delete/:id([0-9]+)', (request, response) => {
-		response.send({
-			title: `${request.params.obj_type} with id ${request.params.id} deleted`
-		})
+	app.get('/:obj_type/delete/:id', (request, response) => {
+		let
+			find_map = {
+				_id: makeMongoId(request.params.id)
+			};
+		dbHandle.collection(
+			request.params.obj_type,
+			(outer_error, collection) => {
+				let options_map = {
+					safe: true,
+					single: true
+				};
+				collection.remove(
+					find_map,
+					options_map,
+					(inner_error, delete_count) => {
+						response.send({
+							delete_count: delete_count
+						});
+					}
+				);
+			}
+		);
 	});
 }
 
 module.exports = {configRoutes: configRoutes};
+dbHandle.open(() => {
+	console.log('*** Connected to MongoDb ***');
+});
--- scripts/query.js
+++ scripts/query.js
@@ -1,2 +1,54 @@
 use spa;
+
+db.user.insert({
+	"name": "Mike Mikowski",
+	"is_online": false,
+	"css_map": {
+		"top": 100,
+		"left": 120,
+		"background-color": "rgb(136, 255, 136)"
+	}
+});
+
+db.user.insert({
+	"name": "Mr. Joshua",
+	"is_online": false,
+	"css_map": {
+		"top": 150,
+		"left": 120,
+		"background-color": "rgb(136, 255, 136)"
+	}
+});
+
+db.user.insert({
+	"name": "Your name here",
+	"is_online": false,
+	"css_map": {
+		"top": 50,
+		"left": 120,
+		"background-color": "rgb(136, 255, 136)"
+	}
+});
+
+db.user.insert({
+	"name": "Hapless interloper",
+	"is_online": false,
+	"css_map": {
+		"top": 0,
+		"left": 120,
+		"background-color": "rgb(136, 255, 136)"
+	}
+});
 db.user.find();
+
+db.user.update({
+	"_id": ObjectId("57fbf8560369d00046d8ce8c")
+}, {
+	$set: {
+		"name": "Josh Powell"
+	}
+});
+
+db.user.remove({
+	"_id": ObjectId("57fbf84b0369d00046d8ce8a")
+});
